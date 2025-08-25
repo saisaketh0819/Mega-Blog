6ed587182ab902179db074a263873f7e1ddd0db3
saisaketh0819 <saisaketh0819@gmail.com>
2025-08-25 23:31:21 +0530

    ok

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -4,11 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1>Hello Vite!</h1>
+      
     </>
   )
 }
